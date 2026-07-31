008a67db35824c4c2e0578b7b8551079089cd363
AaditAntony~ <aaditantony1112@gmail.com>
2026-07-31 10:19:20 +0530

    the primitive type have been looked after

---
+++ day2-part-2/primaryvariable.py
@@ -0,0 +1,15 @@
+# subscripting
+print("hello"[0])
+
+# String
+print("123"+"456")
+
+# integer= whole number
+print("123_456_78")
+
+# Float = floating point number
+print("42342.5")
+
+# boolean
+print(True)
+print(False)
