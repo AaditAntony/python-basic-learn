c64e2ea0e05f184216ffac9c0047f41ef05db923
AaditAntony~ <aaditantony1112@gmail.com>
2026-08-03 15:14:49 +0530

    the code of the type conversion is done

--- day2-part-2/type_check_error_conversation.dart.py
+++ day2-part-2/type_check_error_conversation.dart.py
@@ -1,8 +1,10 @@
+len("hello")
 
-
-
-
-
+print(type("abc"))
+print(type(123))
+print(type(3.45))
+print(type(True))
+print("number of letters in your name :" +str(len(input("enter your name"))))
 
 
 
