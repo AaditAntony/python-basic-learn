689036a0de206a1307df106ae2103e336bde0c1e
AaditAntony~ <aaditantony1112@gmail.com>
2026-08-03 15:42:05 +0530

    the code is done

---
+++ day2-part-2/mathematical_operators.py
@@ -0,0 +1,11 @@
+print(1+2)
+print(3-2)
+print(3*2)
+print(5/3)
+print(5//3)  # this one is removes decimal after devison
+print(2**3)  #  this one is power
+
+#PEMDASLR
+# paramerter exponent multiplicaton division addition substraction left and right
+
+print((3*4)+78+89/4)
