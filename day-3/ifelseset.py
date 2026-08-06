2b9767645e4a5039c6ccfc2e2f23c2a4308e632d
AaditAntony~ <aaditantony1112@gmail.com>
2026-08-06 15:52:06 +0530

    the code of the elif is done

--- day-3/ifelseset.py
+++ day-3/ifelseset.py
@@ -4,5 +4,16 @@ height = int(input("enter your height in cm: "))
 if height>=120:
     print("you can ride the roller coster")
 
+    age=int(input("enter your age in years: "))
+    if age<=12:
+        print("You can not ride the roller coster")
+    elif age <= 18:
+        print("You can ride the roller coster pay the $7")
+    else:
+        print("You can ride the roller coster pay the $10")
+
+
+
+
 else :
     print("you can not ride the roller coster")
