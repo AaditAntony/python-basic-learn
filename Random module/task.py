b337e4e7b1af9a813ce1b777d736c837393985bb
AaditAntony~ <aaditantony1112@gmail.com>
2026-08-07 16:36:06 +0530

    the code of the random float is done

--- Random module/task.py
+++ Random module/task.py
@@ -5,3 +5,9 @@ random_integer=random.randint(1,10)
 print(random_integer)
 
 print(my_module.my_favourate_number)
+
+random_number= random.random()*10
+print(random_number)
+
+random_floaat= random.uniform(1,10)
+print(random_floaat)
