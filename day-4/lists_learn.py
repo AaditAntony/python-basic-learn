1df369722aa52fab929427f8dc97fc177a29a744
AaditAntony~ <aaditantony1112@gmail.com>
2026-08-07 17:25:25 +0530

    list extend is done

--- day-4/lists_learn.py
+++ day-4/lists_learn.py
@@ -3,3 +3,5 @@ state_of_america = ["delware","pencilvaniya","new jerssy"]
 print(state_of_america[1])
 state_of_america.append("angelaland")
 print(state_of_america)
+state_of_america.extend(["Angelaland","jack blue land"])
+print(state_of_america)
