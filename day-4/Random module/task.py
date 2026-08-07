30142d656c00242e7fd328b253cc00ee06560532
AaditAntony~ <aaditantony1112@gmail.com>
2026-08-07 16:55:34 +0530

    the program is done of the random program heads and tail

--- day-4/Random module/task.py
+++ day-4/Random module/task.py
@@ -1,13 +1,23 @@
 import random
-import  my_module
+# import  my_module
+#
+# random_integer=random.randint(1,10)
+# print(random_integer)
+#
+# print(my_module.my_favourate_number)
+#
+# random_number= random.random()*10
+# print(random_number)
+#
+# random_floaat= random.uniform(1,10)
+# print(random_floaat)
 
-random_integer=random.randint(1,10)
-print(random_integer)
 
-print(my_module.my_favourate_number)
+print("heads")
+print("tails")
 
-random_number= random.random()*10
-print(random_number)
-
-random_floaat= random.uniform(1,10)
-print(random_floaat)
+random_heads_or_tails= random.randint(0,1)
+if random_heads_or_tails == 0:
+    print("heads")
+else :
+    print("tails")
