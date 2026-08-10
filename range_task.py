19fcf1f2f3512aa454d36f21b2b1aa1b3b628304
AaditAntony~ <aaditantony1112@gmail.com>
2026-08-10 16:42:15 +0530

    the code of the range task is done

--- range_task.py
+++ range_task.py
@@ -1,5 +1,9 @@
-total = 0;
-for numbers in range ( 1,101):
-    total +=numbers
-print(total)
-
+for value in range(1, 101):
+    if value % 3 == 0 and value % 5 == 0:
+        print("FizzBuzz")
+    elif value % 3 == 0:
+        print("Fizz")
+    elif value % 5 == 0:
+        print("Buzz")
+    else:
+        print(value)
